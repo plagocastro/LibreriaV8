29afba3a9ba1bd0132ea6b6a3e1a2e0dae54a917
Pablo Lago Castro <plagocastro@danielcastelao.org>
2021-05-10 14:13:56 +0200

    Añadimos metodos a la clase

--- src/com/COD/Main.java
+++ src/com/COD/Main.java
@@ -1,10 +1,18 @@
 package com.COD;
 
 
+import LIBRERIAINOUT.FactoriaDeSalidas;
+import LIBRERIAINOUT.InterfaceMetodosComunes;
 
 public class Main {
 
     public static void main(String[] args) {
+        InterfaceMetodosComunes unaConsola = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.CONSOLA, "Hola");
+        unaConsola.visualiza();
+
+        InterfaceMetodosComunes unaVentana = FactoriaDeSalidas.getProducto(FactoriaDeSalidas.VENTANA,"Chao");
+        unaVentana.visualiza();
+
 
     }
 }
